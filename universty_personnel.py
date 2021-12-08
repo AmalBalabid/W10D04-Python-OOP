63f553859f39e085326a218cd52de0c18477fae0
S-MW <72529306+S-MW@users.noreply.github.com>
2021-12-08 14:38:32 +0300

    Add seter & geter

--- universty_personnel.py
+++ universty_personnel.py
@@ -1,3 +1,4 @@
+# --------------- universty_personnel class ------------------
 class universty_personnel:
     def __init__ (self ,id,name,university_id,university_email):
         self.id = id
@@ -7,6 +8,31 @@ class universty_personnel:
     
     def Personal_Information(self):
         return f"{self.id} - {self.name} - {self.university_id} - {self.university_email}"
+    
+    def set_id(self,id):
+        self.id=id
 
-u_p = universty_personnel(437,"Saad",437015722,"salwuhayb@imamu.edu.sa")
+    def get_id(self):
+        return self.id
+
+    def set_name(self,name):
+        self.name=name
+        
+    def get_name(self):
+        return self.name
+
+    def set_university_id(self,university_id):
+        self.university_id=university_id
+        
+    def get_university_id(self):
+        return self.university_id
+
+    def set_university_email(self,university_email):
+        self.university_email=university_email
+        
+    def get_university_email(self):
+        return self.university_email
+
+u_p = universty_personnel(437,"Saad",437000000,"salwu@###.edu.sa")
+u_p.set_university_email("test@gmail.com")
 print(u_p.Personal_Information())
